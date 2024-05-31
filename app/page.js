21cf66348583774584ec13645ee94cbc8fc61ad4
chintamani-pala <chintamanipala67@gmail.com>
2024-05-31 13:35:53 +0530

    Security added and Update Home component

--- app/page.js
+++ app/page.js
@@ -1,3 +1,58 @@
+// import { personalData } from "@/utils/data/personal-data";
+// import AboutSection from "./components/homepage/about";
+// import Blog from "./components/homepage/blog";
+// import ContactSection from "./components/homepage/contact";
+// import Education from "./components/homepage/education";
+// import Experience from "./components/homepage/experience";
+// import HeroSection from "./components/homepage/hero-section";
+// import Projects from "./components/homepage/projects";
+// import Skills from "./components/homepage/skills";
+// import Github from "./components/homepage/github";
+
+// async function getData() {
+//   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
+
+//   if (!res.ok) {
+//     throw new Error('Failed to fetch data')
+//   }
+
+//   const data = await res.json();
+
+//   const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
+
+//   return filtered;
+// };
+
+
+
+// export default async function Home() {
+//   const blogs = await getData();
+ 
+
+
+
+//   return (
+//     <>
+//       <HeroSection />
+//       <AboutSection />
+//       <Projects />
+//       <Skills />
+//       <Experience />
+//       <Education />
+//       <Blog blogs={blogs} />
+//       <Github />
+//       <ContactSection />
+//     </>
+//   )
+// };
+
+
+
+
+
+
+"use client"
+import React, { useEffect, useState } from 'react';
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import Blog from "./components/homepage/blog";
@@ -8,12 +63,13 @@ import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 import Github from "./components/homepage/github";
+import security from "./security"
 
 async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
+  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`);
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data')
+    throw new Error('Failed to fetch data');
   }
 
   const data = await res.json();
@@ -21,10 +77,19 @@ async function getData() {
   const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
 
   return filtered;
-};
+}
 
-export default async function Home() {
-  const blogs = await getData();
+export default function Home() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const blogsData = await getData();
+      setBlogs(blogsData);
+    }
+    fetchData();
+    security()
+  }, []);
 
   return (
     <>
@@ -38,5 +103,5 @@ export default async function Home() {
       <Github />
       <ContactSection />
     </>
-  )
-};
+  );
+}
