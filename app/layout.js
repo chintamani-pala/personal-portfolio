c5ab3081b96af61c1d919d102b4394ed49cdea17
chintamani-pala <chintamanipala67@gmail.com>
2024-06-01 12:26:12 +0530

    seo optimization

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: `Portfolio of ${personalData.name} - Software Developer`,
-  description: `This is the portfolio of ${personalData.name}. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.`,
+  description: `Welcome to ${personalData.name}'s portfolio. I'm a self-taught full stack developer passionate about learning and collaboration. Let's create innovative web solutions together..`,
 };
 
 export default function RootLayout({ children }) {
