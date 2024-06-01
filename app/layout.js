108f9d7b5532afeb3bbcc6829ae1350a50cc9eb9
chintamani-pala <chintamanipala67@gmail.com>
2024-06-01 12:42:08 +0530

    seo optimization

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,7 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
   title: `Portfolio of ${personalData.name} - Software Developer`,
   description: `Welcome to ${personalData.name}'s portfolio. I'm a self-taught full stack developer passionate about learning and collaboration. Let's create innovative web solutions together..`,
+  keywords:"chintamani pala , full stack web developer, chintamani , pala, web developer , backend developer, frontend developer, software developer"
 };
 
 export default function RootLayout({ children }) {
