d85e82f410c13b6441092cc1497f40bf37a92db5
CHINTAMANI PALA <chintamanipala67@gmail.com>
2024-06-22 02:28:36 +0530

    Update index.jsx

--- app/components/homepage/blog/index.jsx
+++ app/components/homepage/blog/index.jsx
@@ -2,6 +2,7 @@
 import Link from 'next/link';
 import { FaArrowRight } from 'react-icons/fa';
 import BlogCard from './blog-card';
+import { personalData } from "@/utils/data/personal-data";
 import React, { useEffect, useState } from 'react';
 function Blog() {
   const [blogs,setBlogs]=useState([]);
