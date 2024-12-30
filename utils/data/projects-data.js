44c0a6abc18c8f485284567b35e4f0b92d5c1315
CHINTAMANI PALA <chintamanipala67@gmail.com>
2024-12-30 10:14:16 +0530

    Update projects-data.js

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -6,9 +6,31 @@ import travel from '/public/image/travel.jpg';
 export const projectsData = [
     {
         id: 1,
+        name: 'A AI online code compiler',
+        description: "An intelligent, multi-language coding platform with real-time suggestions, error detection, and auto-completion. Fully responsive and designed for seamless use on any device, it boosts productivity for developers of all levels.",
+        tools: ['monaco-editor-react','framer-motion','convex','zustand','Next.js', 'JSX', 'Tailwind css', 'SweetAlert', 'react-icons'],
+        role: 'Full Stack Developer',
+        code: '',
+        demo: '',
+        github:"https://github.com/chintamani-pala/AI-Code-Editor",
+        live:"https://code.chintamanipala.in/",
+    },
+    {
+        id: 2,
+        name: 'Codemate-collaborative-code-editor',
+        description: "A collaborative code editor powered by Socket.io, enabling real-time teamwork with seamless communication and live coding features.",
+        tools: ['React.js', 'JSX', 'Tailwind css', 'SweetAlert', 'react-icons', 'express.Js','socket.io','react-hot-toast'],
+        role: 'Full Stack Developer',
+        code: '',
+        demo: '',
+        github:"https://github.com/chintamani-pala/realtime-code-editor",
+        live:"https://codemate-chintamani.onrender.com/",
+    },
+    {
+        id: 3,
         name: 'A Personal Portolio',
         description: "Welcome to my portfolio powered by Next.js, EmailJS, and Tailwind CSS. Explore a showcase of my work and achievements in an elegant and efficient design crafted for seamless navigation. With Next.js, the site offers blazing-fast performance and dynamic rendering, ensuring a smooth user experience. ",
-        tools: ['Next.js', 'JSX', 'Tailwind', 'SweetAlert', 'react-icons'],
+        tools: ['Next.js', 'JSX', 'Tailwind css', 'SweetAlert', 'react-icons'],
         role: 'Full Stack Developer',
         code: '',
         demo: '',
@@ -16,7 +38,7 @@ export const projectsData = [
         live:"https://chintamanipala.in/",
     },
     {
-        id: 2,
+        id: 4,
         name: 'A ELearning Management System',
         description: "Our eLearning Management System (LMS) is a comprehensive platform designed to streamline and enhance the learning experience for educators and learners alike. With powerful features and intuitive functionality, our LMS empowers organizations to deliver engaging and effective online courses, training programs, and educational content.",
         tools: ['React', 'JSX', 'Tailwind', 'Express Js', 'Typescript', 'Radis', 'MongoDB'],
@@ -27,7 +49,7 @@ export const projectsData = [
         live:"https://learn.chintamanipala.in/",
     },
     {
-        id: 3,
+        id: 5,
         name: 'A Responsive Weather Viewer',
         description: "Introducing our Weather Viewer – your go-to destination for real-time weather updates!. Stay ahead of the weather curve with our intuitive web application. Explore current conditions and forecasts with ease, whether you're planning a weekend adventure or just staying informed for your daily commute",
         tools: ['HTML','CSS','JS',"JQuery"],
@@ -38,7 +60,7 @@ export const projectsData = [
         live:"https://weather-app-using-html-css-js.vercel.app/",
     },
     {
-        id: 4,
+        id: 6,
         name: 'Tic Tac Toe Game',
         description: 'Tic Tac Toe is a classic game enjoyed by people of all ages. Whether you\'re a seasoned player looking for a quick match or a newcomer eager to learn, our Tic Tac Toe game offers endless entertainment for everyone.',
         tools: ['HTML','CSS','JS',"JQuery"],
@@ -49,7 +71,7 @@ export const projectsData = [
         live:"https://tic-tac-toe-game-using-html-css-js.vercel.app/",
     },
     {
-        id: 5,
+        id: 7,
         name: 'Simple Quiz Game',
         description: 'Challenge yourself and your friends with our captivating quiz game, designed to entertain, educate, and inspire players of all ages. Whether you\'re a trivia aficionado or just looking for a fun way to pass the time, our Quiz Game offers an immersive and engaging experience for everyone.',
         tools: ['HTML','CSS','JS',"JQuery"],
